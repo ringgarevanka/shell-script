9ad310ce2573b5fb4dbc1b752a0a7ecf08541e83
Ringga Revanka <99320495+ringgarevanka@users.noreply.github.com>
2025-04-06 19:56:40 +0700

    initial-commit

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# shell-script
-This repository contains a `Shell script` that I created.
-See other branches in this repository!
+# shell-script/tree/get-big.LITTLE
+## GET big.LITTLE
+Get `big core` and `LITTLE core` amount and frequency (MHz) on Android device using [Android Debug Bridge (ADB)](https://wikipedia.org/wiki/Android_Debug_Bridge)
+---
+## How to Use
+1. Clone this repository branch or download `get_bigLITTLE.sh` files in repository
+2. Enable [Developer Options](https://www.google.com/search?q=How+to+Enable+Developer+Options)
+3. There are two ways to use `USB Debugging` and `Wireless Debugging`.
+
+### USB Debugging
+1. [Enable and Connect USB Debugging](https://www.google.com/search?q=How+to+enable+and+connect+USB+Debugging+to+PC)
+2. Run the `get_bigLITTLE.sh` shell script
+``` sh
+adb shell sh "/path/to/file/get_bigLITTLE.sh"
+```
+
+### Wireless Debugging
+1. [Enable Wireless Debugging](https://www.google.com/search?q=How+to+Enable+Wireless+Debugging)
+2. [Connect Wireless Debugging](https://www.google.com/search?q=How+to+Connect+Wireless+Debugging) or Using [Brevent App](https://play.google.com/store/apps/details?id=me.piebridge.brevent) ([HELP!](https://www.google.com/search?q=How+To+Connect+Brevent)), [Termux (GitHub or F-Droid Recommended)](https://github.com/termux/termux-app) + [Shizuku](https://github.com/RikkaApps/Shizuku) ([HELP!](https://www.google.com/search?q=How+To+Connect+Shizuku+To+Termux)), etc.
+3. Run the `get_bigLITTLE.sh` shell script
+``` sh
+adb shell sh "/path/to/file/get_bigLITTLE.sh"
+```
+or (if you use Brevent, etc.)
+``` sh
+sh "/path/to/file/get_bigLITTLE.sh"
+```
+---
---
+++ get_bigLITTLE.sh
@@ -0,0 +1,83 @@
+#!/system/bin/sh
+
+# Global variabel untuk output
+little_cores=0
+big_cores=0
+little_avg=0
+big_avg=0
+
+get_bigLITTLE() {
+    local total_clusters=0
+    local little_freq_total=0
+    local big_freq_total=0
+
+    # --- 1. Collect the maximum frequencies of all CPUs ---
+    local all_freqs=$(cat /sys/devices/system/cpu/*/cpufreq/cpuinfo_max_freq 2>/dev/null | xargs)
+
+    # --- 2. Group unique frequencies ---
+    local sorted_freqs freq_prev freq count
+    sorted_freqs=$(tr ' ' '\n' <<<"$all_freqs" | sort -n)
+    freq_prev=""
+    for freq in $sorted_freqs; do
+        if [ "$freq" != "$freq_prev" ]; then
+            count=$(grep -c "^$freq$" <<<"$sorted_freqs")
+            eval freq_${total_clusters}=$freq
+            eval count_${total_clusters}=$count
+            total_clusters=$((total_clusters + 1))
+            freq_prev=$freq
+        fi
+    done
+
+    # --- 3. big.LITTLE classification based on the number of clusters ---
+    local i=0 f c f0 f1 c0 c1
+
+    if [ "$total_clusters" -gt 2 ]; then
+        while [ "$i" -lt "$total_clusters" ]; do
+            eval f=\$freq_$i
+            eval c=\$count_$i
+            if [ "$i" -lt 2 ]; then
+                little_cores=$((little_cores + c))
+                little_freq_total=$((little_freq_total + f * c))
+            else
+                big_cores=$((big_cores + c))
+                big_freq_total=$((big_freq_total + f * c))
+            fi
+            ((i++))
+        done
+
+    elif [ "$total_clusters" -eq 2 ]; then
+        eval f0=\$freq_0
+                    eval c0=\$count_0
+        eval f1=\$freq_1
+                    eval c1=\$count_1
+
+        if [ "$f0" -lt "$f1" ]; then
+            little_cores=$c0
+            little_freq_total=$((f0 * c0))
+            big_cores=$c1
+            big_freq_total=$((f1 * c1))
+        else
+            little_cores=$c1
+            little_freq_total=$((f1 * c1))
+            big_cores=$c0
+            big_freq_total=$((f0 * c0))
+        fi
+
+    elif [ "$total_clusters" -eq 1 ]; then
+        eval f0=\$freq_0
+                    eval c0=\$count_0
+        big_cores=$c0
+        big_freq_total=$((f0 * c0))
+    fi
+
+    # --- 4. Calculate the average frequency in MHz ---
+    little_avg=$((little_cores > 0 ? little_freq_total / little_cores / 1000 : 0))
+    big_avg=$((big_cores > 0 ? big_freq_total / big_cores / 1000 : 0))
+}
+
+# Call function
+get_bigLITTLE
+
+# Output
+echo "big core: $big_cores ($big_avg MHz)"
+echo "LITTLE core: $little_cores ($little_avg MHz)"
